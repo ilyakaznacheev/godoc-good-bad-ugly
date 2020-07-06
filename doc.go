cf950493ff297f697238288168b23119fb6e1016
Ilya Kaznacheev <ilya@kaznacheev.me>
2020-07-07 01:12:57 +0300

    Add doc

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Package main
+
+For a great documentation you don’t need a book, you need a guide
+
+- with explicit entry point,
+- and real use-cases and tips,
+- and a lot of examples!
+*/
+package main
